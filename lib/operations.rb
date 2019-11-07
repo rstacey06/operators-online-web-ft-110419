cd1860888088a4609b6716ba560bd2014fb34c51
Burbage Stacey <rstacey06@gmail.com>
2019-11-07 13:09:39 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,11 @@
 def unsafe?(speed)
- if speed < 40
-   true 
- elsif speed > 60
-   true
- else #else statement here because dosent evaluate true
- false 
+if speed > 60 
+  
  end
  end
 
 def not_safe?(speed)
-	speed < 40 || speed > 60 ? true : false 
+
 end
 	
 
